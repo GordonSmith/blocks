56a22dd2eb64ce2019bc1e3bbe64075ccd1e138e
Gordon Smith <GordonSmith@users.noreply.github.com>
2022-11-22 19:33:13 +0000

    feat: Data Preview
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

---
+++ blocks/data-file-block/index.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Box, ToggleSwitch } from "@primer/react";
+import { FileBlockProps, getLanguageFromFilename } from "@githubnext/blocks";
+import { compile, omd2notebook, ojs2notebook } from "@hpcc-js/observablehq-compiler";
+import { Runtime, Inspector } from "@observablehq/runtime";
+
+import "@hpcc-js/observablehq-compiler/dist/index.css";
+
+export default function (props: FileBlockProps) {
+    const { block, context, metadata, onUpdateMetadata } = props;
+    const language = Boolean(context.path) ? getLanguageFromFilename(context.path) : "N/A";
+    const [isOn, setIsOn] = React.useState(true)
+
+    const notebookRef = React.useRef(null);
+
+    React.useEffect(() => {
+        if (isOn && notebookRef.current) {
+            let rawNotebook;
+            if ((context.path.indexOf(".csv") === context.path.length - 4) ||
+                (context.path.indexOf(".json") === context.path.length - 5) ||
+                (context.path.indexOf(".parquet") === context.path.length - 8)) {
+                rawNotebook = ojs2notebook(`\
+import { viewof tableSummary, viewof jsQueryEditor } with { myData as data } from "@randomfractals/duckdb-data-tables";
+
+viewof tableSummary;
+
+viewof searchResults = Inputs.search(myData);
+Inputs.table(searchResults);
+md\`
+## Data Wrangler
+
+Query and transform DuckDB **${context.path}** data with [Arquero](https://uwdata.github.io/arquero/) in Observable [Data Wrangler](https://observablehq.com/@observablehq/data-wrangler) cell with auto-generated JS data transformations code:\`
+viewof jsQueryEditor;
+
+md\`-- - \`;
+
+import {DuckDBClient} from '@cmudig/duckdb';
+
+file = FileAttachment("https://raw.githubusercontent.com/${context.owner}/${context.repo}/${context.sha}/${context.path}");
+
+db = DuckDBClient.of([file])
+
+name = (await db.describeTables())[0].name;
+
+myData = db.sql([\`SELECT * FROM '\${name}'\`]);
+`);
+            }
+            if (rawNotebook) {
+                compile(rawNotebook)
+                    .then(nb => {
+                        new Runtime().module(nb, Inspector.into(notebookRef.current));
+                    })
+                    .catch(e => { })
+                    ;
+            }
+        }
+    }, [block, language, isOn, notebookRef.current]);
+
+    const onClick = () => {
+        setIsOn(!isOn)
+    }
+
+    return <>
+        <div ref={notebookRef}></div>
+    </>;
+
+}
